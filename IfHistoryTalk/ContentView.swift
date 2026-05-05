807d3cea536a069ce1b76f770b18f0cd7b990e53
P040631 <skt.P040631@partner.sktelecom.com>
2026-05-05 20:05:12 +0900

    Initial Commit

---
+++ IfHistoryTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IfHistoryTalk
+//
+//  Created by park sanghoon on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IfHistoryTalk/IfHistoryTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IfHistoryTalkApp.swift
+//  IfHistoryTalk
+//
+//  Created by park sanghoon on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct IfHistoryTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
